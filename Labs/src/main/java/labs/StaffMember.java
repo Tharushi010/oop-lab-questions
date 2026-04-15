7970312271e6b991b7548ff7db3a536773f0b8f2
Tharushi <274221045+Tharushi010@users.noreply.github.com>
2026-04-15 23:44:08 +0530

    changed StaffMember location

---
+++ Labs/src/main/java/labs/StaffMember.java
@@ -0,0 +1,35 @@
+
+package labs;
+
+public abstract class StaffMember {
+
+   private String fullName; 
+   private final String staffID; 
+   
+   protected String department; 
+   public StaffMember(String fullName, String staffID, String Department) 
+   { 
+       this.fullName = fullName; 
+       this.staffID = staffID; 
+       this.department = department; 
+   } 
+   public String getFullName() 
+   { 
+       return fullName; 
+   } 
+   public String getStaffID() 
+   { 
+       return staffID; 
+   } 
+   public String getDepartment() 
+   {
+       return department; 
+   }
+   public final void displayBasicDetails()
+   {
+       System.out.println("Full name: " + fullName); 
+       System.out.println("Staff ID: " + staffID); 
+       System.out.println("Department: " + department); 
+   }
+    public abstract double calculateMonthlyPayment();  
+}
